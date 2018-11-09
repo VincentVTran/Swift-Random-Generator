43a23d724032462d4b93c421113332f32d090ab3
Vincent Tran <VincentTran@vincents-macbook.local>
2018-11-09 11:31:16 -0600

    Initial Commit

---
+++ Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Application
+//
+//  Created by Vincent Tran on 11/9/18.
+//  Copyright © 2018 Vincent Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
